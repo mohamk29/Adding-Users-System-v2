eae9f5ca4cc0c748c329148dccb775f1432dc55d
mohamk29 <mohamk29@my.yorku.ca>
2023-09-13 16:41:49 -0400

    Created the add users component to set up the form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AdddUsers from "./components/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AdddUsers />
     </div>
   );
 }
---
+++ src/components/AddUser.js
@@ -0,0 +1,13 @@
+const AdddUsers = (props) => {
+  return (
+    <>
+      <label>Username</label>
+      <input type="string" />
+      <label>Age (Years)</label>
+      <input type="number" />
+      <button>Add Users</button>
+    </>
+  );
+};
+
+export default AdddUsers;
