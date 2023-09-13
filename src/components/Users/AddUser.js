52fd34dab76ec2a130129800723fa2050bd482f2
mohamk29 <mohamk29@my.yorku.ca>
2023-09-13 18:23:26 -0400

    Created the button component and managed form input state

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,36 @@
 import Card from "./../UI/Card";
+import Button from "./../UI/Button";
 import classes from "./AddUsers.module.css";
+import { useState } from "react";
 
 const AdddUsers = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const usernameHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.input}>
           <label htmlFor="username">Username</label>
-          <input type="text" />
-          <label htmlFor="age">Age (Years)</label>
-          <input type="text" />
-        </div>
+          <input type="text" onChange={usernameHandler} />
 
-        <button>Add Users</button>
+          <label htmlFor="age">Age (Years)</label>
+          <input type="text" onChange={ageHandler} />
+
+          <Button type="submit">Add Users</Button>
+        </div>
       </form>
     </Card>
   );
